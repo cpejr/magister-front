137218f8e8f55282a78625584987c6543b68cc47
debora <deboramuzzi@cpejr.com.br>
2025-10-11 10:12:23 -0300

    style: ultimas estilizações do header

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,17 +1,24 @@
 import { useNavigate } from "react-router-dom"
-import {Botao, StyledHeader, StyledLink, Divider, Logo} from "./styles"
-
+import {Botao, StyledHeader, StyledLink, Divider, Logo, ContainerLink} from "./styles"
+import logo from "../Logo/logo.png"
 export default function Header() {
     const navigate = useNavigate();
     return (
      <StyledHeader>
-        <Logo></Logo>
+        <Logo>
+        <img
+            src={logo}
+            alt="Logo"
+            style={{ width:  150, height: 78}}
+        />
+        </Logo>
+        <ContainerLink>
         <StyledLink to ="/" >Quem somos?</StyledLink>
          <Divider>|</Divider>
         <StyledLink to = "/texto"> Dixit</StyledLink>
          <Divider>|</Divider>
         <StyledLink to = "/gamificação">Labore</StyledLink>
-                  
+        </ContainerLink>
         <Botao onClick={() => navigate("/login")}>Login</Botao>
      </StyledHeader>
     )
--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const StyledHeader= styled.div`
+export const StyledHeader= styled.header`
 height: 90px;
 width: 100%;
 background-color:  #F0E7B0;
@@ -9,30 +9,34 @@ color: black;
 display: flex;
 align-items: center;
 font-size: 30px;
-padding: 0 50px;
-justify-content: space-between;
+position: relative;
+  padding: 0 50px;
 
 `
 export const ContainerLink = styled.div`
 display: flex;
 align-items: center;
-gap: 20px;
+justify-content: center;
+position: absolute;
+transform: translateX(-50%);
+left: 50%;
+gap: 10px;
+
+
 `
 export const Botao = styled.button`
-
 background-color: #D5C97B;
 width: 100px;
 height: 50px;
-font-size: 20px;
+font-size: 18px;
 font-family: 'Palatino Linotype', 'Book Antiqua', Palatino, serif;
 border: none;
 border-radius: 10px;
-margin-bottom: 30px;
 color: black;
-margin-top:30px;
 margin-right: 10px;
-  
+margin-left: 80%;
 `;
+
 export const Divider = styled.span`
   color: rgba(0, 0, 0, 0.5);
   font-weight: lighter;
@@ -41,14 +45,11 @@ export const Divider = styled.span`
 export const Logo= styled.div`
   display: flex;
   align-items: center;
-  gap: 10px;
-  font-family: "Merriweather";
-  font-weight: bold;
-  font-size: 24px;
+
 `; 
 export const StyledLink = styled(Link)`
     text-decoration: none;
-    font-size: 20px;
+    font-size: 18px;
     font-family: 'Palatino Linotype', 'Book Antiqua', Palatino, serif;
     color: black;
     transition: color 0.3s;
