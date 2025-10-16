408768a0baf54984b6b8bdccd18fd83f03178877
matheusmadeira <fumacammadeira@gmail.com>
2025-10-16 14:06:19 -0300

    fix: linking error: ´text error´

--- src/Pages/Texto/Texto.jsx
+++ src/Pages/Texto/Texto.jsx
@@ -1,45 +1,51 @@
 import { useState } from "react";
 import Header from "../../Components/Header/header";
-import { Botao1, Botao2, Container, Input , Transcrito} from "./styles";
+import { Botao1, Botao2, Container, Input, Transcrito } from "./styles";
 import { useTranscreverTexto } from "../../Hooks/useTranscricao";
 
+export default function Texto() {
+  const [text, setText] = useState("");
 
-export default function Texto(){
-    const [text, setText] = useState("");
-    
-    const {mutate: transcreverTexto, data, isLoading, isError} = useTranscreverTexto({
-        onSuccess : (text) => {
-             console.log("Transcrição recebida!")
-        },
-        onError: () => { console.log("Erro")}
-    })
+  const {
+    mutate: transcreverTexto,
+    data,
+    isLoading,
+    isError,
+  } = useTranscreverTexto({
+    onSuccess: (text) => {
+      console.log("Transcrição recebida!");
+    },
+    onError: () => {
+      console.log("Erro");
+    },
+  });
 
-    const handleTranscricao  = () => {
-        if(text){
-            transcreverTexto({text});
-        }
+  const handleTranscricao = () => {
+    if (text) {
+      transcreverTexto({ text });
     }
-    console.log(text);
-    return ( 
-        <Container>
-            <Header></Header>
+  };
+  console.log(text);
+  return (
+    <Container>
+      <Header></Header>
 
-            <Input 
-            type="text" 
-            placeholder="Escreva aqui..."
-            value={text}
-            onChange={(e) => setText(e.target.value) }
-            ></Input>
+      <Input
+        type="text"
+        placeholder="Escreva aqui..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></Input>
 
-            <Botao1 onClick={handleTranscricao} disabled={isLoading}>
-                {isLoading ? "Transcrevendo..." : "Transcrever"}
-            </Botao1>
+      <Botao1 onClick={handleTranscricao} disabled={isLoading}>
+        {isLoading ? "Transcrevendo..." : "Transcrever"}
+      </Botao1>
 
-            <Transcrito>
-                {isError && "Erro na transcrição"}
-                {data ? data.resultado : "Tradução..."}
-            </Transcrito>
-            <Botao2>Voltar</Botao2>
-        </Container>
-    )
+      <Transcrito>
+        {isError && "Erro na transcrição"}
+        {data ? data.resultado : "Tradução..."}
+      </Transcrito>
+      <Botao2>Voltar</Botao2>
+    </Container>
+  );
 }
