a1fd19ba1c682d17158c6507d96bdb014921c4ec
medes <arquimedesfreitas@cpejr.com.br>
2025-10-23 21:56:40 -0300

    fix: responsividade para mobile

--- src/Pages/Questions/styles.js
+++ src/Pages/Questions/styles.js
@@ -1,4 +1,3 @@
-// src/pages/Questions/QuestionsStyle.js
 import styled from "styled-components";
 
 export const MainArea = styled.main`
@@ -9,6 +8,14 @@ export const MainArea = styled.main`
   flex-direction: column;
   align-items: center;
   padding-top: 60px;
+
+  @media (max-width: 768px) {
+    padding-top: 50px;
+  }
+
+  @media (max-width: 480px) {
+    padding-top: 40px;
+  }
 `;
 
 export const Title = styled.h1`
@@ -16,4 +23,20 @@ export const Title = styled.h1`
   margin-bottom: 32px;
   color: #4a2803;
   font-family: "Merriweather", serif;
+  text-align: center;
+
+  @media (max-width: 1024px) {
+    font-size: 32px;
+    margin-bottom: 24px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 28px;
+    margin-bottom: 20px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 24px;
+    margin-bottom: 16px;
+  }
 `;
